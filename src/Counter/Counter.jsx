06594c82ddcf8d5a5709c009f152051030789dfd
Shubham6865 <paradeshubham6865@gmail.com>
2024-12-26 13:32:44 +0530

    counter and todo

---
+++ src/Counter/Counter.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import './counter.css';
+const Counter = () => {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    if (count < 10) {
+
+      setCount(count + 1);
+    }
+  };
+  const handleDecrement = () => {
+    if (count > 0) {
+
+      setCount(count - 1);
+    }
+  };
+  return (
+
+    <>
+      <div className="container">
+
+        <h3 className="title">Counter which can increase max to 10 and min 0</h3>
+
+        <div className="Counter">
+          <button onClick={handleDecrement}>Decrement</button>
+          <h1>{count}</h1>
+          <button onClick={handleIncrement}>Increment</button>
+        </div>
+        <div className="Counter">
+          <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default Counter;
---
+++ src/ToDo/ToDo.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const Todo = () => {
+
+  const [tasks, setTasks] = useState([]);
+  const [taskTitle, setTaskTitle] = useState();
+  const [taskDetail, setTaskDetail] = useState();
+
+  const handleAdd = () => {
+    const newTask = {
+      id: Date.now(),
+      Title: taskTitle,
+      Detail: taskDetail
+    }
+
+    setTasks([...tasks, newTask]);
+    setTaskTitle('');
+    setTaskDetail('');
+  }
+
+  const handleDelete = (id) => {
+
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  }
+  const handelDeleteAll = () => {
+    setTasks([]);
+
+  }
+  return (
+    <div>
+
+      <h1>Todo App</h1>
+
+      <div>
+        <input type="text" placeholder="Enter Title" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} />
+        <input type="text" placeholder="Enter Task Detail" value={taskDetail} onChange={(e) => setTaskDetail(e.target.value)} />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <div><button onClick={() => handelDeleteAll()}>
+        Delete All
+      </button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <h3>{task.Title}</h3>
+            <h3>{task.Detail}</h3>
+            <button onClick={() => handleDelete(task.id)}>Delete</button>
+
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Todo;
